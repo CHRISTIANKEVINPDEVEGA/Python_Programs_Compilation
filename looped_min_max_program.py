4a4d7da4ca58d54eb69d81da985dc200b9b3a38b
Christian <Christiandevega12@gmail.com>
2024-03-10 15:48:56 +0800

    added loop and input

--- looped_min_max_program.py
+++ looped_min_max_program.py
@@ -1,11 +1,31 @@
-def minmax(input_dict):
-    maxVal = None
-    minVal = None
-    for val in input_dict:
-        if maxVal is None or val > maxVal:
-            maxVal= val
-        if minVal is None or val < minVal:
-            minVal = val
+def minmax(maxVal, minVal, val):
+    if maxVal is None or val > maxVal:
+        maxVal= val
+    if minVal is None or val < minVal:
+        minVal = val
     return maxVal, minVal
 
-print(minmax([2,3,0,4,5]))
+maxVal = None
+minVal = None
+
+while True:
+    userInput = input("Enter a number: ")
+    try:
+        number = int(userInput)
+        maxVal_eval , minVal_eval = minmax(maxVal, minVal, number)
+        maxVal = maxVal_eval
+        minVal = minVal_eval
+        continue
+
+    except:
+        if userInput == "done":
+            print(f"Max Value: {maxVal}, Min Value: {minVal}")
+            break
+        else:
+            print("Invalid, Please Enter a number")
+            continue
+
+        # userInput = input("Enter a number: ")
+        # number = int(userInput)
+        # minmax(number)
+        # continue
