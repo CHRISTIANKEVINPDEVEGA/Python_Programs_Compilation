8f112c71387355c0c9907a73e770c2bcb84368fe
Christian <Christiandevega12@gmail.com>
2024-03-15 21:47:16 +0800

    added code for exercise 2 - avrage float in text

--- writefilepython.py
+++ writefilepython.py
@@ -9,7 +9,7 @@ print(fout.write(line2))
 
 fout.close()
 
-# Exercise one
+# Exercise 1
 input = input('Enter a filename: ')
 
 try:
@@ -19,5 +19,28 @@ except:
     exit()
 
 for line in uphand:
-    print(line.upper())
+    print(line.upper().rstrip())
 
+
+# Exercise 2
+
+input2 = input('Enter a filename: ')
+
+try:
+    uphand2 = open(input2)
+except:
+    print('Invalid name')
+    exit()
+
+count = 0
+floatcount = 0
+
+for line in uphand2:
+    if line.startswith('X-DSPAM-Confidence:'):
+        count += 1
+        icolon = line.find(':')
+        floatval = float(line[(icolon+1):])
+        floatcount += floatval
+
+floatavg = floatcount/count
+print('Average spam confidence: ', floatavg)
