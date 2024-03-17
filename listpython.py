760e2d1135834acdb3c550fc57497a2d5f1ffe29
Christian <Christiandevega12@gmail.com>
2024-03-17 17:22:05 +0800

    add code for deleting list elements

--- listpython.py
+++ listpython.py
@@ -54,3 +54,16 @@ print(t1)
 t = ['a','c','d','b']
 t.sort()
 print(t)
+
+t = ['a', 'b', 'c']
+x = t.pop(2)
+print(t)
+print(x)
+del t[1]
+print(t)
+t.remove('a')
+print(t)
+
+t =  ['a', 'b', 'c', 'd', 'e', 'f']
+del t[1:3]
+print(t)
