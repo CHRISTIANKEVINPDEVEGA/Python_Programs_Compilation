573ca240ad12630abac8dee5e43d1a80606fe59f
Christian <Christiandevega12@gmail.com>
2024-03-19 23:47:29 +0800

    added code for looping in lines to find unique words

--- romeo_wordscanner.py
+++ romeo_wordscanner.py
@@ -1,2 +1,20 @@
 title_name = input('Enter file: ')
 fhand = open(title_name)
+
+unique_words = []
+
+for line in fhand:
+    line = line.rstrip()
+    word_list = line.split()
+    for word in word_list:
+        if len(unique_words) == 0:
+            unique_words.append(word)
+        else:
+            if word not in unique_words :
+                unique_words.append(word)
+            else:
+                continue
+
+unique_words.sort()
+
+print(unique_words)
