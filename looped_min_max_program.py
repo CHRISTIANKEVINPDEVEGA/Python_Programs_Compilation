a96077a9d5ee0b8b44a34c3812542e6050f41d1f
Christian <Christiandevega12@gmail.com>
2024-03-21 15:17:14 +0800

    rewrote code for minmax looping program to apply list

--- looped_min_max_program.py
+++ looped_min_max_program.py
@@ -1,31 +1,40 @@
-def minmax(maxVal, minVal, val):
-    if maxVal is None or val > maxVal:
-        maxVal= val
-    if minVal is None or val < minVal:
-        minVal = val
-    return maxVal, minVal
+# def minmax(maxVal, minVal, val):
+#     if maxVal is None or val > maxVal:
+#         maxVal= val
+#     if minVal is None or val < minVal:
+#         minVal = val
+#     return maxVal, minVal
 
-maxVal = None
-minVal = None
+# maxVal = None
+# minVal = None
 
+# while True:
+#     userInput = input("Enter a number: ")
+#     try:
+#         number = int(userInput)
+#         maxVal_eval , minVal_eval = minmax(maxVal, minVal, number)
+#         maxVal = maxVal_eval
+#         minVal = minVal_eval
+#         continue
+
+#     except:
+#         if userInput == "done":
+#             print(f"Max Value: {maxVal}, Min Value: {minVal}")
+#             break
+#         else:
+#             print("Invalid, Please Enter a number")
+#             continue
+
+
+storeCount = []
 while True:
-    userInput = input("Enter a number: ")
+    numVal = input('Enter a number: ')
     try:
-        number = int(userInput)
-        maxVal_eval , minVal_eval = minmax(maxVal, minVal, number)
-        maxVal = maxVal_eval
-        minVal = minVal_eval
+        numVal = float(numVal)
+        storeCount.append(numVal)
         continue
-
     except:
-        if userInput == "done":
-            print(f"Max Value: {maxVal}, Min Value: {minVal}")
+        if numVal == 'done':
             break
         else:
-            print("Invalid, Please Enter a number")
-            continue
-
-        # userInput = input("Enter a number: ")
-        # number = int(userInput)
-        # minmax(number)
-        # continue
+            print('Invalid input')
