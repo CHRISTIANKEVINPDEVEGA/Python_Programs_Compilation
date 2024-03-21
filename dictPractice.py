8646992b0b67de85c2df49c409b0eb4c495b5462
Christian <Christiandevega12@gmail.com>
2024-03-21 18:23:45 +0800

    added code for looping and dictionaries

--- dictPractice.py
+++ dictPractice.py
@@ -63,3 +63,16 @@ for line in fhand:
         else:
             counts[word] += 1
 print(counts)
+
+counts = { 'chuck' : 1 , 'annie' : 42, 'jan': 100}
+
+for key in counts:
+    if counts[key] > 10:
+        print(key, counts[key]) 
+
+lst = list(counts.keys())
+print(lst)
+lst.sort()
+print(lst)
+for key in lst:
+    print(key,counts[key])
