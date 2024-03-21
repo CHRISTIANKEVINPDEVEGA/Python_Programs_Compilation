d54567e575f9135b02a671f88efc6908b874dee1
Christian <Christiandevega12@gmail.com>
2024-03-21 15:08:24 +0800

    added code for counting all lines that start with 'from'

--- fromcount.py
+++ fromcount.py
@@ -4,12 +4,15 @@ def parserline(linesofword):
 
     
 txtfile = input('Enter Filename: ')
+count = 0
 try:
- linesOffile = open(txtfile)
- for lineOffile in linesOffile:
-    linenospace = str(lineOffile.rstrip())
-    if not linenospace.startswith('From '):
-        continue
-    parserline(linenospace)
+    linesOffile = open(txtfile)
+    for lineOffile in linesOffile:
+        linenospace = str(lineOffile.rstrip())
+        if not linenospace.startswith('From '):
+            continue
+        parserline(linenospace)
+        count += 1
+    print('There were',count,'lines in the file with From as the first word')
 except:
  print('File Not found')
