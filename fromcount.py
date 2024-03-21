0d959a938fd13e18d89a99cb66f9fbf7d68e6fa9
Christian <Christiandevega12@gmail.com>
2024-03-21 15:05:02 +0800

    added code for parsing looping each line in .txt file

--- fromcount.py
+++ fromcount.py
@@ -1,11 +1,15 @@
 def parserline(linesofword):
-    linenospace = linesofword.rstrip()
-    words = linenospace.split()
-    print(words[2])
+    words = linesofword.split()
+    print(words[1])
 
     
 txtfile = input('Enter Filename: ')
 try:
- lineOffile = txtfile.open()
+ linesOffile = open(txtfile)
+ for lineOffile in linesOffile:
+    linenospace = str(lineOffile.rstrip())
+    if not linenospace.startswith('From '):
+        continue
+    parserline(linenospace)
 except:
  print('File Not found')
