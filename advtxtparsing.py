68d64d95b608fe080d15521435a560b15e044403
Christian <Christiandevega12@gmail.com>
2024-03-21 19:19:21 +0800

    added code for printing the keys

--- advtxtparsing.py
+++ advtxtparsing.py
@@ -13,4 +13,9 @@ for line in fhand:
     line = line.translate(line.maketrans("","",string.punctuation))
     line = line.lower()
     words = line.split()
-    
+    for word in words:
+        if word not in counts:
+            counts[word]=1
+        else:
+            counts[word] += 1
+print(counts) 
