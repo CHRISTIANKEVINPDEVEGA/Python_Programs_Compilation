2cd91c5fa8a4e9c3293ba771de4749d4e5e94635
Christian <Christiandevega12@gmail.com>
2024-03-21 16:38:31 +0800

    added code for craeting a unique key from words in txt

--- dictPractice.py
+++ dictPractice.py
@@ -9,3 +9,19 @@ print(len(eng2sp))
 print('one' in eng2sp)
 print('uno' in eng2sp.values())
 print(type(list(eng2sp.values())))
+
+
+dict_wordtxt = {}
+linestxt = open('words.txt')
+for line in linestxt:
+    line = line.rstrip()
+    words_line = line.split()
+    for word in words_line:
+        if len(dict_wordtxt) == 0:
+            dict_wordtxt[word] = len(word)
+        if word in dict_wordtxt:
+            continue
+        else:
+            dict_wordtxt[word] = len(word)
+
+print(dict_wordtxt)
