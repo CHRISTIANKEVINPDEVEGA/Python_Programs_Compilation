38eaa6b2d22bc8ae8813c8575e3c7e16b16f964b
Christian <Christiandevega12@gmail.com>
2024-03-21 19:43:48 +0800

    added code for parsing lines to getting the histogram of email and its frequency of message

--- pythondow.py
+++ pythondow.py
@@ -12,3 +12,17 @@ for line in fhand:
         weekdayCount[linelst[2]] = 1
 
 print(weekdayCount)
+
+emailmesCount = dict()
+fhand = open('word.txt')
+for line in fhand:
+    line = line.rstrip()
+    if not line.startswith('From: '):
+        continue 
+    linelst = line.split()
+    if linelst[1] in emailmesCount:
+        emailmesCount[linelst[1]] += 1
+    else:
+        emailmesCount[linelst[1]] = 1
+
+print(emailmesCount)
