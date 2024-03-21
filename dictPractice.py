643bdfd20e8a493508dbbb4b8bb3f79d0bf34867
Christian <Christiandevega12@gmail.com>
2024-03-21 16:22:42 +0800

    explore more dict function

--- dictPractice.py
+++ dictPractice.py
@@ -4,4 +4,8 @@ eng2sp['one'] = 'uno'
 print(eng2sp)
 
 eng2sp = {'one':'uno', 'two':'dos', 'three':'tres'}
-print(eng2sp)
+print(eng2sp['two'])
+print(len(eng2sp))
+print('one' in eng2sp)
+print('uno' in eng2sp.values())
+print(type(list(eng2sp.values())))
