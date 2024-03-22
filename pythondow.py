b15a63a992efa4d1625e8e0d151477eeec378df1
Christian <Christiandevega12@gmail.com>
2024-03-22 23:17:13 +0800

    revise the code

--- pythondow.py
+++ pythondow.py
@@ -25,28 +25,34 @@ for line in fhand:
     else:
         emailmesCount[linelst[1]] = 1
 
-print(emailmesCount)
+lst = list()
+for key,val in emailmesCount.items():
+    lst.append((val,key))
+
+lst.sort(reverse=True)
+key,val = lst[0]
+print(val,key)
 
 
-maxVal = None
-for keys in emailmesCount:
-    if maxVal is None or emailmesCount[maxVal] <= emailmesCount[keys]:
-        maxVal = keys
+# maxVal = None
+# for keys in emailmesCount:
+#     if maxVal is None or emailmesCount[maxVal] <= emailmesCount[keys]:
+#         maxVal = keys
 
-print(maxVal,emailmesCount[maxVal])
+# print(maxVal,emailmesCount[maxVal])
 
-domainnameCount = dict()
-fhand = open('word.txt')
-for line in fhand:
-    line = line.rstrip()
-    if not line.startswith('From '):
-        continue
-    at_index = line.find('@')
-    at_space = line.find(" ",at_index)
-    domainname = line[(at_index+1):at_space]
-    if domainname in domainnameCount:
-        domainnameCount[domainname] += 1
-    else:
-        domainnameCount[domainname] = 1
+# domainnameCount = dict()
+# fhand = open('word.txt')
+# for line in fhand:
+#     line = line.rstrip()
+#     if not line.startswith('From '):
+#         continue
+#     at_index = line.find('@')
+#     at_space = line.find(" ",at_index)
+#     domainname = line[(at_index+1):at_space]
+#     if domainname in domainnameCount:
+#         domainnameCount[domainname] += 1
+#     else:
+#         domainnameCount[domainname] = 1
 
-print(domainnameCount)
+# print(domainnameCount)
